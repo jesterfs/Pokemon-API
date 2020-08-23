ee317928cce762eb70069b59f639fc8eaa8bac29
Stewart Jester <jesterstewart@gmail.com>
2020-08-22 20:14:34 -0700

    prep for deployment

--- server.js
+++ server.js
@@ -1,16 +1,78 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const helmet = require('helmet')
+const cors = require('cors')
+
+const POKEDEX = require('./pokedex.json')
+
 
 const app = express()
 
-app.use(morgan('dev'))
 
-app.use((req, res) => {
-  res.send('Hello, wally!')
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
+
+app.use(helmet())
+app.use(cors())
+
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+
+// const { name , type } = req.query
+
+app.use(function validateBearerToken(req, res, next) {
+    // const bearerToken = req.get('Authorization').split(' ')[1]
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+   
+    // move to the next middleware
+    next()
+  })
+
+
+function handleGetTypes(req, res) {
+    res.json(validTypes)
+    }
+    
+app.get('/types', handleGetTypes)
+
+
+
+app.get('/pokemon' , function handleGetPokemon(req, res) {
+  let response = POKEDEX.pokemon;
+
+  if (req.query.name) {
+    response = response.filter(pokemon =>
+      pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
+    )
+}
+
+if (req.query.types) {
+  response = response.filter(pokemon => 
+      pokemon.type.includes(req.query.type)
+    )
+}
+
+res.json(response)
 })
 
-const PORT = 8000
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = {error: {message: 'server error'}}
+  } else {
+    response = {error}
+  }
+  res.status(500).json(response)
+})
+
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
 })
